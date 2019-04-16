57519bc1363b4b29c73ae0e475b987b0d6f6961f
Vicent Perez <vicennt@gmail.com>
2019-04-16 21:00:48 +0100

    Cleaning shiny default code

--- code/app.R
+++ code/app.R
@@ -1,48 +1,32 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+# ---- Final Degree Thesis ----
+# Alumno: Vicent Pérez
+# Tutor: Cèsar Ferri
+# Universitat Politécnica de València
 #
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# User Interface
 ui <- fluidPage(
+
+   # Title
+   titlePanel("Final Degree Thesis"),
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
+   # Layout (two colums: map & info about station)
+   sidebarLayout(position = "right",
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         #TODO: Map of bicycle stations
       ),
-      
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         #TODO: Exclusive info about the selected station
       )
    )
 )
 
-# Define server logic required to draw a histogram
+# Server function
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+   #TODO: Handle inputs and outputs
 }
 
 # Run the application 
