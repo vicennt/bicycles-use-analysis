205c3d74b4e12d162432531276060715ff2c06fe
Vicent Perez <vicennt@gmail.com>
2019-06-09 23:31:16 +0100

    Little bug fixed

--- code/scripts/merge_all_data.R
+++ code/scripts/merge_all_data.R
@@ -1,6 +1,4 @@
 #Merging all files into one
-print("Script para fusionar los datos")
-print(getwd())
 dataset_cities <- read.csv("../../datasets/cities.csv")
 cities <- dataset_cities$NAME
 columns <-c("city","station","month","year","hour","houred","totinc","totdecr","medbikes","meanbikes","lastbikes","propempty","propfull","count")
@@ -11,7 +9,6 @@ for(city in cities){# Each city
   dir <- list.files(pattern = city)
   for(d in dir){ # Each station
     setwd(d)
-    print(getwd())
     file_name <- list.files()
     filename_splited <- strsplit(d, ":")
     station <- filename_splited[[1]][2]
@@ -20,9 +17,8 @@ for(city in cities){# Each city
     aux <- cbind(city = city, aux)
     result <- rbind(result, aux)
     setwd("..")
-    print(getwd())
   }
   print(paste0("City ", city, " done!"))
 }
-setwd("../data_merged")
-write.csv(result, "all_bicycle_data.csv", row.names = FALSE)
+write.csv(result, "../data_merged/all_bicycle_data.csv", row.names = FALSE)
+print("Files merged!")
