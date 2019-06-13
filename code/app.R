d95c0e3481c447fecaad1609ce678bcb7f0f884d
Vicent Perez <vicennt@gmail.com>
2019-06-13 16:14:27 +0100

    Weather data aggregated by day

--- code/app.R
+++ code/app.R
@@ -28,9 +28,10 @@ cities <- NULL
 cities_names <- NULL
 bicycles_dict <<- hash()
 weather_dict <<- hash()
+weather_dict_agg <<- hash()
 usage_city <- NULL
 usage_station <- NULL
-selected_weather_day_agg <<- hash()
+
 
 
 
@@ -40,7 +41,9 @@ load_data <- function(){
   cities_names <<- cities$NAME
   for(c in cities_names){
     bicycles_dict[[c]] <<- read.csv(file=paste0("../datasets/data_merged/cities/",c,"/",c,".csv"), header=TRUE, sep=",")
-    weather_dict[[c]] <<- read.csv(file=paste0("../datasets/weather_agg_v2/",c,"_agg.csv"), header=TRUE, sep=",")
+    weather_dict[[c]] <<- select(read.csv(file=paste0("../datasets/weather_agg_v2/",c,"_agg.csv"), header=TRUE, sep=","), 
+                                main_temp_max, main_temp_min, wind_speed, rain_3h, snow_3h, day, month, year, hour, weather_main)
+    weather_dict_agg[[c]] <<- agg_weather_data_by_day(weather_dict[[c]])
   }
   #Creating new dataframe with station demand info
   usage_station <- data.frame(matrix(ncol = 3, nrow = 0))
@@ -50,11 +53,23 @@ load_data <- function(){
   colnames(usage_city) <- c("city","average_demand")
 }
 
-
-# ------- DATA TRANSFORMATION ---------
-
-
-load_data()
+agg_weather_data_by_day <- function(df_weather_city){
+  subset <- select(mutate(df_weather_city, date = as.Date(paste0(day,"-",month,"-",year), format = "%d-%m-%Y")),
+                   main_temp_max, main_temp_min, wind_speed, rain_3h, snow_3h, weather_main, date)
+  dataset_dates <- unique(subset$date)
+  count <- 1
+  df_aux <- data.frame(matrix(ncol = 7, nrow = 0))
+  colnames(df_aux) <- c("main_temp_max", "main_temp_min","wind_speed", "rain_3h", "snow_3h", "weather_main", "date")
+  while(count <= length(dataset_dates)){
+    subframe <- filter(subset, date == dataset_dates[count])
+    aux <- data.frame(main_temp_max = mean(subframe$main_temp_max), main_temp_min = mean(subframe$main_temp_min),
+                      wind_speed = mean(subframe$wind_speed), rain_3h = mean(subframe$rain_3h),
+                      snow_3h = mean(subframe$snow_3h), date = dataset_dates[count], weather = names(which.max(table(subframe$weather_main))))
+    df_aux <- rbind(df_aux, aux)
+    count <- count + 1
+  }
+  df_aux
+}
 
 add_city_usage <- function(c){
   # Adding city usage info
@@ -70,11 +85,9 @@ add_station_usage <- function(c, s){
   usage_station <<- rbind(usage_station, aux)
 }
 
-add_aggregated_weather <-function(){
-  #TODO: 
-}
 
-
+# ------- DATA TRANSFORMATION ---------
+load_data()
 for(c in cities_names){
   add_city_usage(c)
   id_stations <- filter(stations, CITY == c)$NUM_STATION
