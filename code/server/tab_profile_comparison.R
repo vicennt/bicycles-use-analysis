e3564e7ce357b7f59abc3817d31df170ac844425
Vicent Perez <vicennt@gmail.com>
2019-06-27 01:20:00 +0200

    Compare cities: radio button view inserted

--- code/server/tab_profile_comparison.R
+++ code/server/tab_profile_comparison.R
@@ -1,29 +1,45 @@
-# TODO: Profile comparision
+# Profile comparison
 
 output$compare_cities_plot <- renderPlot({
-  # First city
+  view <- input$city_comparation_radio
   city_one <- input$city_one_comparation
-  subset_first_city <- filter(bicycles_dict[[city_one]])
-  subset_first_city <- select(subset_first_city, hour, totdecr)
-  subset_first_city <- subset_first_city %>% group_by(hour) %>% summarise(totdecr = mean(totdecr))
-  
-  # Second city
   city_two <- input$city_two_comparation
-  subset_second_city <- filter(bicycles_dict[[city_two]])
-  subset_second_city <- select(subset_second_city, hour, totdecr)
-  subset_second_city <- subset_second_city %>% group_by(hour) %>% summarise(totdecr = mean(totdecr))
-  
-  df <- data.frame(c(0:23), subset_first_city$totdecr, subset_second_city$totdecr)
+  if(view == 'daily_view'){
+    # First city
+    subset_first_city <- filter(bicycles_dict[[city_one]])
+    subset_first_city <- select(subset_first_city, hour, totdecr)
+    subset_first_city <- subset_first_city %>% group_by(hour) %>% summarise(totdecr = mean(totdecr))
+    # Second city
+    subset_second_city <- filter(bicycles_dict[[city_two]])
+    subset_second_city <- select(subset_second_city, hour, totdecr)
+    subset_second_city <- subset_second_city %>% group_by(hour) %>% summarise(totdecr = mean(totdecr))
+    df <- data.frame(c(0:23), subset_first_city$totdecr, subset_second_city$totdecr)
+  }else if (view == 'weekly_view'){
+    # First city
+    subset_first_city <- filter(bicycles_dict[[city_one]])
+    subset_first_city <- select(subset_first_city, hour, totdecr, weekday)
+    subset_first_city <- subset_first_city %>% group_by(hour, weekday) %>% summarise(totdecr = mean(totdecr))
+    subset_first_city <- subset_first_city[order(subset_first_city$weekday),]
+    # Second city
+    subset_second_city <- filter(bicycles_dict[[city_two]])
+    subset_second_city <- select(subset_second_city, hour, totdecr, weekday)
+    subset_second_city <- subset_second_city %>% group_by(hour, weekday) %>% summarise(totdecr = mean(totdecr))
+    subset_second_city <- subset_second_city[order(subset_second_city$weekday),]
+    df <- data.frame(c(0:167), subset_first_city$totdecr, subset_second_city$totdecr)
+  }
+
   colnames(df) <- c("x","First city","Second city")
   df2 <- melt(data = df, id.vars = "x")
-  plot <- ggplot(data = df2, aes(x = x, y = value, color = variable)) + geom_line(size = 2) +
-    ylab('Demand') + xlab('Hour') + scale_x_continuous(breaks = c(0:23)) + 
-    scale_y_continuous(seq(0,5,0.5)) + scale_color_manual(values=c("#accc5a", "#67cce6")) + 
-    ylim(0,5) + labs(color = "Cities")
-  
+  plot <- ggplot(data = df2, aes(x = x, y = value, color = variable)) +
+    labs(y = "Demand", x = "Hour", color = "Cities") +
+    scale_color_manual(values=c("#accc5a", "#67cce6")) 
+    
+  if(view == 'daily_view'){
+    plot <- plot + geom_line(size = 2, inherit.aes = TRUE) + 
+      scale_x_continuous(breaks = c(0:23)) + scale_y_continuous(seq(0,5,0.5))
+  }else if(view == 'weekly_view'){
+    plot <- plot + geom_line(size = 1, inherit.aes = TRUE) + 
+      scale_x_continuous(breaks = seq(0, 167, 24)) + scale_y_continuous(seq(0,5,0.5))
+  }
   plot
-})
-
-output$compare_cities <- renderPlot({
-  
 })
--- code/ui/tab_profile_comparison_ui.R
+++ code/ui/tab_profile_comparison_ui.R
@@ -1,34 +1,34 @@
 tabItem(tabName = "profile_comparison",
     fluidRow(
-      br(),
-      br(),
       box(
         status = "danger",
         title = "Comparing cities profiles",
         solidHeader = TRUE,
         width = 12,
         fluidRow(
+          br(),
           column(3, 
              box(  
                 status = "info",
                 solidHeader = TRUE,
                 width = 12,    
                 selectInput("city_one_comparation", "Select first city", cities$NAME),
-                selectInput("city_two_comparation", "Select second city", cities$NAME)
+                selectInput("city_two_comparation", "Select second city", cities$NAME),
+                radioButtons("city_comparation_radio", label = "Choose your view",
+                             choices = list("Daily" = "daily_view", "Weekly" = "weekly_view"), selected = "daily_view")
              )
           ),
           column(9, plotOutput("compare_cities_plot")))
       )
     ),
     fluidRow(
-      br(),
-      br(),
       box(
         status = "danger",
         title = "Comparing station profiles",
         solidHeader = TRUE,
         width = 12,
         fluidRow(
+          br(),
           column(3,
             box(  
                 status = "info",
@@ -43,6 +43,13 @@ tabItem(tabName = "profile_comparison",
               width = 12,    
               selectInput("station_two_comparation_city", "Select second city", cities$NAME),
               selectInput("station_two_comparation_station", "Select station", cities$NAME)
+            ),
+            box(  
+              status = "info",
+              solidHeader = TRUE,
+              width = 12,  
+              radioButtons("station_comparation_radio", label = "Choose your view",
+                         choices = list("Daily" = "daily_view", "Monthly" = "monthly_view"), selected = "daily_view")
             )
           ),
           column(9,
